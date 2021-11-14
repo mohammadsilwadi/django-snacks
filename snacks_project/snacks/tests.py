4aad38f687cf5b877baec1df859075b9e07e9886
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-14 14:55:20 +0200

    add home and about page

---
+++ snacks_project/snacks/tests.py
@@ -0,0 +1,25 @@
+from django.test import SimpleTestCase
+from django.http import response
+from django.urls import reverse
+
+# Create your tests here.
+class HomeTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url=reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    def test_home_page_template(self):
+        url = reverse("home")
+        response=self.client.get(url)
+        self.assertTemplateUsed(response,"home.html")  
+        
+        
+class AboutUsTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url=reverse('about_us')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    def test_home_page_template(self):
+        url = reverse("about_us")
+        response=self.client.get(url)
+        self.assertTemplateUsed(response,"about_us.html")  
